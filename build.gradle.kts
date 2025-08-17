737acf297b8305fba890fc25305ade40f49c8510
Akash <ajcodes.tech@gmail.com>
2025-08-17 20:20:45 +0530

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.ajcode404"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ai.koog:koog-agents:0.3.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
